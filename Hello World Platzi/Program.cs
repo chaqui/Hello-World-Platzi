5fdbd7497f46d18097af325642ed0a519cdfa65c
chaqui <josue.chaqui@gmail.con>
2022-01-05 19:16:28 -0600

    Iniciando proyecto

---
+++ Hello World Platzi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World_Platzi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
